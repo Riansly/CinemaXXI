ef4e73aa617634de6a7a6fde9505177b33f3a108
Ylsnair.reiv <rianslyvier@gmail.com>
2024-03-01 11:16:12 +0700

    testing api

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,8 +2,8 @@ import { useEffect, useState } from 'react'
 
 export const useFetch = (apiPath, queryTerm="") => {
 const [data, setData] = useState([]);
-const url = `https://api.themoviedb.org/3/${apiPath}?api_key=3c2e88d662c005d0838192885b5c7fec&query=${queryTerm}&language=en-US&page=1`
-
+const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.API_KEY}&query=${queryTerm}&language=en-US&page=1`
+console.log(url);
 useEffect(()=> {
     async function fetchMovies(){
       const responds = await fetch(url);
